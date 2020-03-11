b1019ceef7b0a101dab7b7560c261a17231b9559
tg686 <1511749967@qq.com>
2020-03-11 13:47:30 +0800

    first framework

---
+++ shop-v-basic/shop-conmon/src/main/java/com/qf/conmon/ShopConmonApplication.java
@@ -0,0 +1,13 @@
+package com.qf.conmon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopConmonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopConmonApplication.class, args);
+    }
+
+}
