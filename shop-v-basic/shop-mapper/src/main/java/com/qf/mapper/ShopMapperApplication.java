c8a36519652eace9f940db593269f943bdd37b39
tg686 <1511749967@qq.com>
2020-03-11 13:41:32 +0800

    first framework

---
+++ shop-v-basic/shop-mapper/src/main/java/com/qf/mapper/ShopMapperApplication.java
@@ -0,0 +1,13 @@
+package com.qf.mapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopMapperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopMapperApplication.class, args);
+    }
+
+}
---
+++ shop-v-basic/shop-mapper/src/test/java/com/qf/mapper/ShopMapperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.mapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopMapperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
