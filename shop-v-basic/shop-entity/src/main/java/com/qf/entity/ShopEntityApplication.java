898b77d4c92f8d9255d6eeaef253d2c57714e740
tg686 <1511749967@qq.com>
2020-03-11 13:22:21 +0800

    first framework

---
+++ shop-v-basic/shop-entity/src/main/java/com/qf/entity/ShopEntityApplication.java
@@ -0,0 +1,13 @@
+package com.qf.entity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopEntityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopEntityApplication.class, args);
+    }
+
+}
---
+++ shop-v-basic/shop-entity/src/test/java/com/qf/entity/ShopEntityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qf.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopEntityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
